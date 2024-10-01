bd3bcc810363bd2b9ccedf34cba3e90897cc971b
unknown <chinhtv124@gmail.com>
2024-10-01 16:30:18 +0700

    add code base

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
-const express = require('express')
+import express from 'express'
+import { mapOrder } from '~/utils/sorts.js'
 
 const app = express()
 
@@ -6,9 +7,23 @@ const hostname = 'localhost'
 const port = 8017
 
 app.get('/', (req, res) => {
-  res.send('<h4>Hello Word</h4>')
+  // Test Absolute import mapOrder
+  // eslint-disable-next-line no-console
+  console.log(mapOrder(
+    [
+      { id: 'id-1', name: 'One' },
+      { id: 'id-2', name: 'Two' },
+      { id: 'id-3', name: 'Three' },
+      { id: 'id-4', name: 'Four' },
+      { id: 'id-5', name: 'Five' }
+    ],
+    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+    'id'
+  ))
+  res.end('<h1>Hello World!</h1><hr>')
 })
 
 app.listen(port, hostname, () => {
-  console.log(`Hello Chinh, I'm running server http://${hostname}:${port}/`)
+  // eslint-disable-next-line no-console
+  console.log(`Hello Chinh, I am running at http://${ hostname }:${ port }/`)
 })
