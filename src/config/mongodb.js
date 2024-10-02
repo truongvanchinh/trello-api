3ede15f647b959366d6eba8de76e1920f63b978a
unknown <chinhtv124@gmail.com>
2024-10-02 17:08:45 +0700

    Connect database, create file env

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,31 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+//eRD8fUCH9SqWImU7 truongchinh708
+import { env } from '~/config/environment'
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+let trelloDatabaseInstance = null
+
+// Khởi tạo 1 đối tượng mongoClientInstance để connect tới MongoDB
+const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+}
+)
+
+// kết nôi tới DB
+export const CONNECT_DB = async () => {
+  // gọi kết nối tới MongoDB Atlas với URI đã khai báo trong thân của mongoClientInstance
+  await mongoClientInstance.connect()
+  trelloDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME)
+}
+
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error('Must connect to Database first')
+  return trelloDatabaseInstance
+}
+
+export const CLOSE_DB = async () => {
+  await mongoClientInstance.close()
+}
--- src/server.js
+++ src/server.js
@@ -1,29 +1,36 @@
+/* eslint-disable no-console */
 import express from 'express'
 import { mapOrder } from '~/utils/sorts.js'
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
+import exitHook from 'async-exit-hook'
+import { env } from '~/config/environment'
+const START_SERVER = () => {
 
-const app = express()
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+  app.get('/', async (req, res) => {
+    console.log(env.AUTHOR)
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  // eslint-disable-next-line no-console
-  console.log(mapOrder(
-    [
-      { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' }
-    ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(`3. Hello Chinh, I am running at http://${ env.APP_HOST }:${ env.APP_PORT }/`)
+  })
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Chinh, I am running at http://${ hostname }:${ port }/`)
-})
+  exitHook(() => {
+    console.log('exiting: ')
+    CLOSE_DB()
+  })
+}
+
+(async () => {
+  try {
+    console.log('1. Connecting DB.........')
+    await CONNECT_DB()
+    console.log('2. Connected DB')
+    START_SERVER()
+  } catch (error) {
+    console.error(error)
+    process.exit(0)
+  }
+})()
