806f9cf7bb87ba6369e0796550a7e5973ad25e65
unknown <chinhtv124@gmail.com>
2024-10-12 11:09:58 +0700

    change about domain deploy

--- src/config/cors.js
+++ src/config/cors.js
@@ -12,7 +12,7 @@ export const corsOptions = {
     // Server Production thì sẽ sửa lại đoạn này thêm một chút nữa để phù hợp với từng môi trường
     // production hoặc dev nhé. Học với mình thì các bạn cứ yên tâm về sự chỉn chu chuẩn chỉnh
     // nhé :D
-    if (!origin && env.BUILD_MODE === 'dev') {
+    if (env.BUILD_MODE === 'dev') {
       return callback(null, true)
     }
     // Kiểm tra dem origin có phải là domain được chấp nhận hay không
