c26c6433f81f4ba05984b8097bbf81ba60625c80
unknown <chinhtv124@gmail.com>
2024-11-14 16:23:50 +0700

    api logout, refresh token

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,7 @@
 import { StatusCodes } from 'http-status-codes'
 import ms from 'ms'
 import { userService } from '~/services/userService'
+import ApiError from '~/utils/ApiError'
 
 const createNew = async (req, res, next) => {
   try {
@@ -42,8 +43,37 @@ const login = async (req, res, next) => {
   } catch (error) { next(error) }
 }
 
+const logout = async (req, res, next) => {
+  try {
+    // Xóa Cookie - ngược lại với hàm login
+    res.clearCookie('accessToken')
+    res.clearCookie('refreshToken')
+
+    res.status (StatusCodes.OK).json({ loggedOut: true })
+  } catch (error) { next(error) }
+}
+
+const refreshToken = async (req, res, next) => {
+  try {
+    const result = await userService.refreshToken(req.cookies?.refreshToken)
+
+    res.cookie('accessToken', result.accessToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+      maxAge: ms('14 days')
+    })
+
+    res.status (StatusCodes.OK).json(result)
+  } catch (error) {
+    next(new ApiError(StatusCodes.FORBIDDEN, 'Please Sign In!!!'))
+  }
+}
+
 export const userController = {
   createNew,
   verifyAccount,
-  login
+  login,
+  logout,
+  refreshToken
 }
